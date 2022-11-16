f1a47ffd2fb3569b755b172540d9c87c41941a5d
P <piotrstachowiak31@gmail.com>
2022-11-16 13:44:23 +0100

    reduced script execution time from over 6 sec to 0.03989982604980469 sec with double random approach instead of merging ranges using itertools chain

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from itertools import chain
 
 # print(random.randrange(1, 9))
 start = time.time()
+
+# próbowałem połączyć przedziały, ale czas wykonywania skryptu przekraczał 6 sekund
 # ranges = list(chain(
 
 #pomysl
@@ -13,53 +15,55 @@ start = time.time()
 #wyszukujemy w ktorym przedziale ten index jest
 #w tym przedziale randomujemy
 
-# ranges = (
-#     range(450000000, 452999999),
-#     range(459000000, 459999999),
-#     range(500000000, 519999999),
-#     range(530000000, 539999999),
-#     range(570000000, 571499999),
-#     range(572000000, 573499999),
-#     range(573900000, 579429999),
-#     range(579500000, 579579999),
-#     range(579600000, 579999999),
-#     range(600000000, 609999999),
-#     range(660000000, 669999999),
-#     range(690000000, 690699999),
-#     range(690800000, 699999999),
-#     range(720000000, 739899999),
-#     range(739910000, 739939999),
-#     range(780000000, 780299999),
-#     range(780700000, 780899999),
-#     range(781000000, 785999999),
-#     range(786080000, 786089999),
-#     range(786100000, 786299999),
-#     range(786500000, 799599999),
-#     range(799700000, 799999999),
-#     range(880000000, 882999999),
-#     range(882200000, 882299999),
-#     range(883000000, 889999999)
-# )
-
 ranges = (
-    range(10, 20),
-    range(30, 40)
+    range(450000000, 452999999),
+    range(459000000, 459999999),
+    range(500000000, 519999999),
+    range(530000000, 539999999),
+    range(570000000, 571499999),
+    range(572000000, 573499999),
+    range(573900000, 579429999),
+    range(579500000, 579579999),
+    range(579600000, 579999999),
+    range(600000000, 609999999),
+    range(660000000, 669999999),
+    range(690000000, 690699999),
+    range(690800000, 699999999),
+    range(720000000, 739899999),
+    range(739910000, 739939999),
+    range(780000000, 780299999),
+    range(780700000, 780899999),
+    range(781000000, 785999999),
+    range(786080000, 786089999),
+    range(786100000, 786299999),
+    range(786500000, 799599999),
+    range(799700000, 799999999),
+    range(880000000, 882999999),
+    range(882200000, 882299999),
+    range(883000000, 889999999)
 )
+
+# ranges = (
+#     range(10, 20),
+#     range(30, 40)
+# )
 
 ranges_length = 0
 for r in ranges: ranges_length += len(r)
-print(ranges_length)
+# print(ranges_length)
 
 index = random.choice(range(1, ranges_length + 1))
-print(index)
+# print(index)
 
 # print(ranges_length - index)
+my_random_range = range
 for r in ranges:
     ranges_length -= len(r)
     if index > ranges_length:
-        print(r)
+        my_random_range = r
         break
 
+print(random.choices(my_random_range, k = 100000))
 # print(*range(1,21))
 
 # merged_ranges = functools.reduce(lambda a, b: a + b, ranges)
