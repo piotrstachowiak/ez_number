8c3341907902df6d98db5e542c3633d9860c0a0e
P <piotrstachowiak31@gmail.com>
2022-12-23 23:26:17 +0100

    interface separated! todo: modes description, docstrings

--- functions.py
+++ functions.py
@@ -3,12 +3,12 @@ import re
 import random
 
 
-def generate_phonebook(amount: int) -> set:
+def generate_phonebook(phonebook_length: int) -> set:
     ranges_length = 0
     for r in data.ranges:
         ranges_length += len(r)
-    numbers = set()
-    while len(numbers) < amount:
+    phonebook = set()
+    while len(phonebook) < phonebook_length:
         ranges_length_copy = ranges_length
         # index is used to pick the range from which a random number will be picked
         # tried simply merging ranges, but it took waaaaaaaaaay too long to process
@@ -16,12 +16,65 @@ def generate_phonebook(amount: int) -> set:
         for r in data.ranges:
             ranges_length_copy -= len(r)
             if index > ranges_length_copy:
-                numbers.add(random.choice(r))
+                phonebook.add(random.choice(r))
                 break
-    return numbers
+    return phonebook
 
 
-def divide_numbers_by_3(phonebook: set, ez_numbers: list) -> tuple:
+def print_user_interface() -> dict:
+    mode, numbers_amount = None, None
+    print("\nWelcome to ez_numbers!\n"
+          "\nAvailable modes are:\n"
+          "1 - The Default - generates 15 numbers in standard mobile number format (e.g. \"881 111 881\").\n"
+          "2 - The Custom - generates up to 100 numbers (you choose how many!) in standard format or consisting of a "
+          "4-digit pattern (e.g.: )).\n"
+          "")
+    while True:
+        try:
+            mode = int(input("\nPlease select mode: "))
+        except ValueError:
+            print("Oops! That was no valid number. Try again!")
+        if mode in [1, 2, 201101430, 3]:
+            break
+        else:
+            print("Available mode commands are: 1, 2, 3.")
+    if mode == 1:
+        numbers_amount = 15
+        print("Well done! Processing...\n")
+    elif mode == 2 or mode == 3:
+        while True:
+            try:
+                numbers_amount = int(input("\nHow many numbers do you want to get? (1 minimum, 100 maximum): "))
+            except ValueError:
+                # print("If only Earl Of Lemongrab saw what you just typed...")
+                print("Oops! That was no valid number. Try again!")
+            if numbers_amount in range(1, 101):
+                print("LETS GO! Processing...\n")
+                break
+    return {
+        "mode": mode,
+        "numbers_amount": numbers_amount
+    }
+
+
+def handle_user_request(phonebook: set, ez_numbers: list, mode: int, numbers_amount: int):
+    final_data = None
+    match mode:
+        case 1:
+            final_data = divide_phonebook_by_3(phonebook, ez_numbers)
+        case 2:
+            half_data = divide_phonebook_by_part_length(phonebook, ez_numbers, 4, 2)
+            final_data = divide_phonebook_by_3(half_data["phonebook"], half_data["ez_numbers"])
+        case 201101430:
+            print("\u210c\U0001D508\u210c\U0001D508\u00A0"*2005*random.randint(21, 37))
+        case 3:
+            half_data = divide_phonebook_by_part_length(phonebook, ez_numbers, 4, 2)
+            final_data = divide_phonebook_by_part_length(half_data["phonebook"], half_data["ez_numbers"], 3, 2)
+    outcome = sorted(final_data["ez_numbers"], key=lambda number: ((len(set(number))), -len(number.split(' '))))
+    print("\n".join(outcome[:numbers_amount]))
+
+
+def divide_phonebook_by_3(phonebook: set, ez_numbers: list) -> dict:
     for number in phonebook.copy():
         current_number = str(number)
         parts = re.findall(".{3}", current_number)
@@ -33,10 +86,13 @@ def divide_numbers_by_3(phonebook: set, ez_numbers: list) -> tuple:
                     ez_numbers.append(current_number)
                     phonebook.remove(number)
                     break
-    return phonebook, ez_numbers
+    return {
+        "phonebook": phonebook,
+        "ez_numbers": ez_numbers
+    }
 
 
-def divide_numbers_by_part_length(phonebook: set, ez_numbers: list, part_length: int, matches: int) -> tuple:
+def divide_phonebook_by_part_length(phonebook: set, ez_numbers: list, part_length: int, matches: int) -> dict:
     for number in phonebook.copy():
         current_number = str(number)
         parts = re.findall(f".{{{part_length}}}", current_number)
@@ -47,51 +103,7 @@ def divide_numbers_by_part_length(phonebook: set, ez_numbers: list, part_length:
                 ez_numbers.append(current_number)
                 phonebook.remove(number)
                 break
-    return phonebook, ez_numbers
-
-
-def initiate_user_interface(phonebook: set, ez_numbers: list):
-    user_input, numbers_amount = 0, 0
-    while True:
-        try:
-            user_input = int(input("\nPlease select mode: "))
-        except ValueError:
-            print("Oops! That was no valid number. Try again!")
-        if user_input not in [1, 2, 201101430, 3]:
-            print("Available mode commands are: 1, 2, 3.")
-        else:
-            break
-
-    if user_input == 1:
-        print("Well done! Processing...\n")
-        divide_numbers_by_3(phonebook, ez_numbers)
-        numbers_amount = 15
-    elif user_input == 201101430:
-        print("\u210c\U0001D508\u210c\U0001D508\u00A0"*2005*random.randint(21, 37))
-    elif user_input == 2 or user_input == 3:
-        while True:
-            try:
-                numbers_amount = int(input("\nHow many numbers do you want to get? (100 maximum): "))
-            except ValueError:
-                # print("If only Earl Of Lemongrab saw what you just typed...")
-                print("Oops! That was no valid number. Try again!")
-            if numbers_amount in range(1, 101):
-                print("LETS GO, processing...\n")
-                break
-        if user_input == 2:
-            aa = divide_numbers_by_part_length(phonebook, ez_numbers, 4, 2)
-            divide_numbers_by_3(*aa)
-        else:
-            divide_numbers_by_part_length(phonebook, ez_numbers, 4, 2)
-            divide_numbers_by_part_length(phonebook, ez_numbers, 3, 2)
-
-
-def handle_user_request(phonebook: set, ez_numbers: list, user_input: int, numbers_amount = 15):
-
-    #switch case here
-
-    outcome = sorted(ez_numbers, key=lambda number: ((len(set(number))), -len(number.split(' '))))
-    print("\n".join(outcome[:numbers_amount]))
-
-# option for user to decide, turns out numbers with 4-digit patterns
-# require more information to be remembered (position of the rest)
+    return {
+        "phonebook": phonebook,
+        "ez_numbers": ez_numbers
+    }
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import functions
 
 phonebook = functions.generate_phonebook(50000)
 ez_numbers = []
+request_data = functions.print_user_interface()
+functions.handle_user_request(phonebook, ez_numbers, request_data["mode"], request_data["numbers_amount"])
